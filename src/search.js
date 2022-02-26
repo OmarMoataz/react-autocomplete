5ad0c726e32d16910c13823986827d394bf3455a
Omar Moataz <omar.moataz@vezeeta.com>
2022-02-26 22:14:25 +0200

    set delayed query that triggers 300 ms after the user finishes typing.

--- src/search.js
+++ src/search.js
@@ -1,17 +1,26 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import useUsersSearch from './hooks/useUsersSearch';
 
 const Search = () => {
   const [query, setQuery] = useState('');
-  const [users, isLoading, isError] = useUsersSearch(query);
+  const [delayedQuery, setDelayedQuery] = useState(query);
+  const [users, isLoading, isError] = useUsersSearch(delayedQuery);
 
-  const handleQueryChange = (e) => {
-    setQuery(e.target.value);
+  const searchTimeout = useRef(null);
+
+  const handleQueryChange = (query) => {
+    setQuery(query);
+    
+    if (searchTimeout.current) clearTimeout(searchTimeout.current);
+
+    searchTimeout.current = setTimeout(() => {
+      setDelayedQuery(query);
+    }, 300);
   }
 
   return (
     <>
-      <input onChange={handleQueryChange} value={query}/>
+      <input onChange={(e) => handleQueryChange(e.target.value)} value={query}/>
       {isLoading && <div> Loading... </div>}
       {users && <ul style={{ padding: '0', listStyleType: 'none' }}>
         {users.map(user => (
